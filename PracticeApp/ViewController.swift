3a5cd767dc065f561f73eaf6e06413904af49670
Hammad Ahmad <en.hammadahmad@gmail.com>
2023-07-20 16:35:10 +0500

    test

--- PracticeApp/ViewController.swift
+++ PracticeApp/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var test = "Testing"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(test)
     }
 
 
