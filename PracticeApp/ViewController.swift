90a0a6800dce47d1d82d2008a5892f707eacfb6e
Hammad Ahmad <en.hammadahmad@gmail.com>
2023-07-20 17:06:43 +0500

    Update ViewController.swift

--- PracticeApp/ViewController.swift
+++ PracticeApp/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let merge = "Merge"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    print("Merge")
     }
 
 
